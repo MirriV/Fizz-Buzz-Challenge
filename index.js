b82413755f98c2f2a7c8f524af5d39ab6927aadc
MirriV <101925718+MirriV@users.noreply.github.com>
2022-07-02 12:02:24 +0200

    Initial setup

---
+++ index.js
@@ -0,0 +1,13 @@
+let myVar =0;
+
+while (myVar <=99){
+    document.getElementById("loop").innerHTML +=
+    myVar + "<br>";
+    myVar ++;
+    if( myVar %3 ==0) {
+        console.log("Fizz")
+    
+        }
+    }
+    console.log(myVar);
+
