31d5d855a756a7630dda908865416690748a86a1
MirriV <101925718+MirriV@users.noreply.github.com>
2022-07-03 19:14:42 +0200

    FizzBuzz now works properly
    Remember to include double brackets around 3 & 5. **An if statement breaks once the output is true and numbers that are divisible by 3 and 5 are also divisible by 15

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
   for (let x=1 ; x<=100; x++){
 
-    if (x%3===0){
+    if (x%15===0){
+      console.log("FizzBuzz");
+    }
+    else if((x%3)===0){
       console.log("Fizz");
     }
-    else if(x%5===0){
+    else if((x%5)===0){
       console.log("Buzz");
-    }
-    else if(x%15===0){
-      console.log("FizzBuzz");
     }
     else{
       console.log(x);
@@ -17,3 +17,26 @@
   
   }
 
+/*for (let i=1; i<=100; i++) {
+
+ // are always divisible by 15
+ // Therefore, "FizzBuzz" is printed in place of that number
+ if (i%15 == 0) {
+ document.write("FizzBuzz" + " ");
+ }
+// "Fizz" is printed in place of numbers
+ // that are divisible by 3
+ else if ((i%3) == 0) {
+ document.write("Fizz" + " ");
+ }
+// "Buzz" is printed in place of numbers
+ // that are divisible by 5
+ else if ((i%5) == 0) {
+ document.write("Buzz" + " ");
+ }
+// If none of the above conditions are satisfied,
+ // the number is printed
+ else {
+ document.write(i + " ");
+ }
+}*/
