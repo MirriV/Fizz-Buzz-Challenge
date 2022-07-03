eacdf321d381e569012bfc88a989d6b386c63a75
MirriV <101925718+MirriV@users.noreply.github.com>
2022-07-03 19:01:48 +0200

    Fizz & Buzz working
    Need to figure out %15

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-let myVar =0;
 
-while (myVar <=99){
-    document.getElementById("loop").innerHTML +=
-    myVar + "<br>";
-    myVar ++;
-    if( myVar %3 ===0) {
-       console.log("Fizz");
-        }
+
+  for (let x=1 ; x<=100; x++){
+
+    if (x%3===0){
+      console.log("Fizz");
     }
-  /* for (x=0 ;x<=100;x++){
-    console.log(x);
-    document.write(x +"<br>");
-
-    if(x%3===0)
-    console.log("Fizz")
-   
-   
-   else if(x%5===0)
-   console.log("Buzz")
-
-   }*/
-   
+    else if(x%5===0){
+      console.log("Buzz");
+    }
+    else if(x%15===0){
+      console.log("FizzBuzz");
+    }
+    else{
+      console.log(x);
+    }
+  
+  }
 
