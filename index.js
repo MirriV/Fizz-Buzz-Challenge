9823641cb80ff8111828da7c61febd61a5fdd7c3
MirriV <101925718+MirriV@users.noreply.github.com>
2022-07-03 08:36:21 +0200

    further updates

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-//let myVar =0;
+let myVar =0;
 
-/*while (myVar <=99){
+while (myVar <=99){
     document.getElementById("loop").innerHTML +=
     myVar + "<br>";
     myVar ++;
     if( myVar %3 ===0) {
        console.log("Fizz");
         }
-    }*/
-   for (x=0 ;x<=100;x++){
+    }
+  /* for (x=0 ;x<=100;x++){
     console.log(x);
     document.write(x +"<br>");
 
     if(x%3===0)
-    console.log("Fizz");
-    
-   }
+    console.log("Fizz")
+   
+   
+   else if(x%5===0)
+   console.log("Buzz")
+
+   }*/
+   
 
